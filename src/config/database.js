8170f4841a5aa97920ab5b9f8c8ac0da9a4ecf5e
wolpique <pomidorkartoshka@gmail.com>
2023-09-07 01:20:46 +0600

    [refactoring] fixed database connection

--- src/config/database.js
+++ src/config/database.js
@@ -7,11 +7,11 @@ const {Client} = require('pg');
 const caCert = fs.readFileSync('ca-certificate.crt');
 
 const db = new Client({
-    host: 'localhost', 
-    port: 5432,
-    database: 'postgres',
-    user: 'postgres',
-    password: "rootUser", 
+    host: process.env.DB_HOST, 
+    port: 25060,
+    database: 'db',
+    user: 'db',
+    password: process.env.DB_PASSWORD, 
     ssl: {
         ca: caCert,
     },
